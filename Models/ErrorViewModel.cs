06f370e060ef2d1c2eecc1907ca0cb2226caf4cc
Tantatorn-dev <timefear007@gmail.com>
2021-03-17 11:41:23 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ozzy_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
