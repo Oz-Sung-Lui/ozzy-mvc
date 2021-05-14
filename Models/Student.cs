0ace364e47ecf8d3c5ca3f9852df923cb13ec970
J4deSPK <jade.supakorn@hotmail.com>
2021-05-14 22:07:50 +0700

    Edit Profile Page v0.5 BY JADE

--- Models/Student.cs
+++ Models/Student.cs
@@ -9,24 +9,24 @@ namespace ozzy_mvc.Models
         public Guid StudentID { get; set; }
 
         [Required(ErrorMessage = "*Username is required.")]
-        [StringLength(10, MinimumLength = 3, ErrorMessage = "*Lenght must between 3 and 10")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "*Lenght must between 3 and 10.")]
         public String Username { get; set; }
 
         [Required(ErrorMessage = "*Firstname is required.")]
-        [StringLength(20, MinimumLength = 3, ErrorMessage = "*Lenght must between 3 and 20")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "*Lenght must between 3 and 20.")]
         public String Firstname { get; set; }
         
         [Required(ErrorMessage = "*Lastname feild is required.")]
-        [StringLength(20, MinimumLength = 3, ErrorMessage = "Lenght must between 3 and 20")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "*Lenght must between 3 and 20.")]
         public String Lastname { get; set; }
         
         [Required(ErrorMessage = "*Password is required.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Password must contain at least 8 characters, including UPPER/lowercase and numbers.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "*Password must contain at least 8 characters, including UPPER/lowercase and numbers.")]
         public String Password { get; set; }
 
         [NotMapped]
         [Required(ErrorMessage = "*Confirm Password is required.")]
-        [Compare("Password", ErrorMessage = "Confirm password doesn't match")]
+        [Compare("Password", ErrorMessage = "*Confirm password doesn't match.")]
         public String ConfirmPassword { get; set; }
 
         public Boolean IsBlacklisted { get; set; }
